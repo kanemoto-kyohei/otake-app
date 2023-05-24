385d47353a6cab3981aac631eea4aadebaebd4ad
金本京平 <kanemotokyouhei@kanemotokyouheinoMacBook-Air.local>
2023-05-24 23:15:28 +0900

    responsivrdeaignを修正

--- resources/js/Pages/App.jsx
+++ resources/js/Pages/App.jsx
@@ -10,9 +10,9 @@ import "./responsivestyle.css";
 const App = () => {
     const { carender_elements, auth, appointments, userId, flash } =
         usePage().props;
-        const isTablet = window.matchMedia("(min-width: 769px) and (max-width: 1024px)").matches;
+        const isTablet = window.matchMedia("(min-width: 1024px) and (max-width: 2800px)").matches;
         const isMobile = window.matchMedia("(max-width: 768px)").matches;
-      
+      console.log(isMobile);
     return (
     
         <AuthenticatedLayout user={auth.user}>
